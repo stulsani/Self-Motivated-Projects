9e85966851e9213d03dc71b792f3caddc8410c5a
Sumeet Tulsani <sumeettulsani1@gmail.com>
2017-04-03 13:03:34 -0700

    TreasureGram Application First Pass

---
+++ Django Projects/Treasuregram/Treasuregram/main_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from . models import Treasure
+
+# Create your views here.
+def index(request):
+    treasures = Treasure.objects.all()
+    return render(request,'index.html', {'treasures': treasures})
+
