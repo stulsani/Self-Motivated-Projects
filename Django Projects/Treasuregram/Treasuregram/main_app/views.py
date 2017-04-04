7f5b823effac4eeeb773bcf55e774a674a888738
Sumeet Tulsani <sumeettulsani1@gmail.com>
2017-04-03 20:53:32 -0700

    Application Completed

--- Django Projects/Treasuregram/Treasuregram/main_app/views.py
+++ Django Projects/Treasuregram/Treasuregram/main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from . models import Treasure
-from . forms import TreasureForm
+from . forms import TreasureForm, LoginForm
 from django.http import HttpResponseRedirect
+from  django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
@@ -16,5 +18,40 @@ def detail(request,treasure_id):
 def post_treasure(request):
     form = TreasureForm(request.POST, request.FILES)
     if form.is_valid():
-        form.save(commit = True)
+        treasure = form.save(commit = False)
+        treasure.user = request.user
+        treasure.save()
+    return HttpResponseRedirect('/')
+
+def profile(request,username):
+    user = User.objects.get(username=username)
+    treasures = Treasure.objects.filter(user=user)
+    return render(request, 'profile.html', {
+        'username': username,
+        'treasures': treasures
+    })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    print("The account was Disabled")
+            else:
+                return HttpResponseRedirect('/login')
+                print("The username and password were incorrect")
+
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
     return HttpResponseRedirect('/')
